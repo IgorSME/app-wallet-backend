1e9ef5e1e54d46fb19b3c7e2e0fb54a8c0b86d1d
Igor Smelyanets <ismelyanets@yahoo.com>
2022-12-13 18:52:45 +0200

    Revert "fix time"
    This reverts commit 83382cd594b965102a351cf4420e435881f52824.

--- helpers/createTokens.js
+++ helpers/createTokens.js
@@ -3,17 +3,15 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY_ACCESS = "", SECRET_KEY_REFRESH = "" } = process.env;
 
 const createToken = (_id) => {
-
-    const payload = {
-        id: _id
-    }
-    const accessToken = jwt.sign(payload, SECRET_KEY_ACCESS, {
-      expiresIn: "2m",
-    });
-    const refreshToken = jwt.sign(payload, SECRET_KEY_REFRESH, {
-      expiresIn: "1w",
-    });
-    return { accessToken, refreshToken }
-}
-module.exports= createToken
-
+  const payload = {
+    id: _id,
+  };
+  const accessToken = jwt.sign(payload, SECRET_KEY_ACCESS, {
+    expiresIn: "2m",
+  });
+  const refreshToken = jwt.sign(payload, SECRET_KEY_REFRESH, {
+    expiresIn: "1w",
+  });
+  return { accessToken, refreshToken };
+};
+module.exports = createToken;
