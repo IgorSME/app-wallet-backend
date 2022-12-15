cd5f073f1e5c36f22b3baaa638d9e2c21f3a5e15
Yevhen Podoliaka <zanp85740@gmail.com>
2022-12-15 07:05:49 +0200

    add totalPagesin response

--- controllers/transactions/getAll.js
+++ controllers/transactions/getAll.js
@@ -1,17 +1,20 @@
 const { Transaction } = require("../../models");
 
 const getAll = async (req, res) => {
-  const { _id } = req.user;
-  const { page = 1, limit = 5 } = req.query;
+  const { _id, userBalance } = req.user;
+  const { page = 1, limit = 5} = req.query;
   const skip = (page - 1) * limit;
 
   const transactions = await Transaction.find({ owner: _id }, "", {
     skip,
     limit,
   }).sort({date:-1});
-  
+  const count = await Transaction.countDocuments({ owner: _id });
+  const totalPages = Math.ceil(count/limit)
   res.json({
     transactions,
+    userBalance,
+    totalPages,
   });
 };
 
